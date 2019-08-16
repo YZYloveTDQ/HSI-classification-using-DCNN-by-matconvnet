917d4e8e6920b87288a106c96154c55d4dbd9859
xiaofei_zh <xiaofei_zh@foxmail.com>
2019-08-16 16:12:54 +0800

    Update cnn_hsi_acc.m

--- cnn_hsi_acc.m
+++ cnn_hsi_acc.m
@@ -1,10 +1,11 @@
 %% Experiment with the cnn_mnist_fc_bnorm
+%% This code is improved to obtian the OA\AA\KAPPA\CA values by Xiaofei Zhang.
 clc;
 clear
 addpath data
 [net_bn, info_bn,OA,AA,Kappa,CA,accu_martix] = cnn_HSI('expDir', 'data/mnist-bnorm', 'batchNormalization', true);
 
-% load ('E:\œ¬‘ÿ◊ ¡œ\mnist_matconvnet\data\mnist-bnorm\imdb.mat');
+% load ('E:\√è√Ç√î√ò√ó√ä√Å√è\mnist_matconvnet\data\mnist-bnorm\imdb.mat');
 load ('.\data\mnist-bnorm\imdb.mat');
 test_index = find(images.set==3);
 test_data = images.data(:,:,:,test_index);
